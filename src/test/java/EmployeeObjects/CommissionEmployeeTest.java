eae1fada0d98f3e9150ef98bce38b79f9fe3ee95
crgutzmann <164046103+crgutzmann@users.noreply.github.com>
2024-03-21 11:53:08 -0500

    Commission employee tests
    we out

--- src/test/java/EmployeeObjects/CommissionEmployeeTest.java
+++ src/test/java/EmployeeObjects/CommissionEmployeeTest.java
@@ -1,1 +1,35 @@
 package EmployeeObjects;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CommissionEmployeeTest {
+    @Test
+    public void testIncreaseSalesPositive() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+        emp.increaseSales(1000.0);
+        assertEquals(1000.0, emp.getSales());
+    }
+
+    @Test
+    public void testIncreaseSalesNegative() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+        emp.increaseSales(-500.0);
+        assertEquals(0.0, emp.getSales());
+    }
+
+    @Test
+    public void testHolidayBonus() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+        double bonus = emp.holidayBonus();
+        assertEquals(0.0, bonus);
+    }
+    @Test
+    public void testAnnualRaise() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+        emp.annualRaise();
+        emp.annualRaise();
+        assertEquals(0.0305, emp.getRate());
+    }
+}
